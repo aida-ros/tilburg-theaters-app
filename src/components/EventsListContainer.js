6fcc16d8fbf6762fca145ac5bb2dc30f5ad47840
AidaRos95 <aidajros@gmail.com>
2019-07-24 11:01:21 +0200

    Data fetched componentDidMount

--- src/components/EventsListContainer.js
+++ src/components/EventsListContainer.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import EventsList from './EventsList';
 
 class EventsListContainer extends Component {
+  componentDidMount = () => {
+    this.fetchEvents();
+  }
+
+  fetchEvents = () => {
+    fetch('https://www.theaterstilburg.nl/api/public/events')
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(console.error)
+  }
+  
   render () {
     return (
       <EventsList/>

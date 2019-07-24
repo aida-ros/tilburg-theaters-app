cac937d053027c7f8fd9f56cf1b0c9baa0eed707
AidaRos95 <aidajros@gmail.com>
2019-07-24 14:13:06 +0200

    Genres are filtered during fetch and added to redux state

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -4,13 +4,24 @@ const initialState = {
   genres: []
 }
 
+const filterGenres = events => {
+  const genres = [];
+  events.filter(event => {
+    if (!genres.includes(event.genre)) {
+      return genres.push(event.genre)
+    }
+  });
+  return genres
+}
+
 const events = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_ALL_EVENTS':
       return {
         ...state,
         allEvents: action.payload,
-        selectedEvents: action.payload
+        selectedEvents: action.payload,
+        genres: filterGenres(action.payload)
       }
     default:
       return state

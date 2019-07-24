e97b07aeb9387564e654dba9157182a9663d548f
AidaRos95 <aidajros@gmail.com>
2019-07-24 11:53:00 +0200

    selectedEvents are rendered to page

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 
 class EventsList extends Component {
-  render () {
-    const {
-      eventsPerTen
+  renderEvents = events => {
+    if (!events) {
+      return <p>Loading events...</p>
+    } else {
+      return events.map(event => {
+        return <div key={event.id} className='event-container'>
+          <h4>{event.title}</h4>
+          <h5>{event.performer}</h5>
+          <h5>{event.location}</h5>
+          <img alt='performer' src={event.images[0]}/>
+          <p>{event.startsAt}</p>
+        </div>
+      })
+    }
+  }
+  
+  render() {
+    const { eventsPerTen
     } = this.props
-    console.log('EVENTS PER TEN', eventsPerTen)
+
     return (
       <main className="events-list-container">
-          <div className='main-header'>
-            <h1>Tilburg Theaters</h1>
-            <h3>Programma overzicht</h3>
-          </div><br/>
-  
-  
-  
-          <h2>All events should be rendered here</h2>
+        <div className='main-header'>
+          <h1>Tilburg Theaters</h1>
+          <h3>Programma overzicht</h3>
+        </div><br />
+
+        {this.renderEvents(eventsPerTen)}
+
+
+
+        <h2>All events should be rendered here</h2>
       </main>
     );
   }
